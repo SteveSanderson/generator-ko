04cd30f215719b71a491fb795fdc9959629e6b48
Steven Sanderson <steve@codeville.net>
2014-05-08 17:45:26 +0100

    Tidy up component template

--- component/templates/viewmodel.js
+++ component/templates/viewmodel.js
@@ -4,10 +4,9 @@ define(['knockout', 'text!./<%= filename %>.html'], function(ko, templateMarkup)
     this.message = ko.observable('Hello from the <%= name %> component!');
   }
 
-  <%= viewModelClassName %>.prototype.dispose = function() {
-    // This runs when the component is torn down. Put here any logic necessary to clean up,
-    // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
-  };
+  // This runs when the component is torn down. Put here any logic necessary to clean up,
+  // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
+  <%= viewModelClassName %>.prototype.dispose = function() { };
   
   return { viewModel: <%= viewModelClassName %>, template: templateMarkup };
 
