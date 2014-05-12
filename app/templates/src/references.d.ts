e8604c47d4a3ec9e9c5799b90bcfdac2cfee90b8
Steven Sanderson <steve@codeville.net>
2014-05-12 18:09:53 +0100

    Include .d.ts for Knockout Projections

--- app/templates/src/references.d.ts
+++ app/templates/src/references.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="app/definitions/jquery/jquery.d.ts" />
 /// <reference path="app/definitions/knockout/knockout.d.ts" />
 /// <reference path="app/definitions/knockout/knockout-3.2.0.d.ts" />
+/// <reference path="app/definitions/knockout-projections/knockout-projections.d.ts" />
 /// <reference path="app/definitions/bootstrap/bootstrap.d.ts" />
 /// <reference path="app/definitions/crossroads/crossroads.d.ts" />
 /// <reference path="app/definitions/hasher/hasher.d.ts" />
