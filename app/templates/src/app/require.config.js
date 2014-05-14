d22eb6cb6f5edbd4c33bd5574b56aa8053a25d70
Steven Sanderson <steve@codeville.net>
2014-05-14 10:54:11 +0100

    Get KO from Bower

--- app/templates/src/app/require.config.js
+++ app/templates/src/app/require.config.js
@@ -2,16 +2,14 @@
 var require = {
     baseUrl: ".",
     paths: {
-        "text":                 "bower_modules/requirejs-text/text",
-        "jquery":               "bower_modules/jquery/dist/jquery",
         "bootstrap":            "bower_modules/components-bootstrap/js/bootstrap.min",
         "crossroads":           "bower_modules/crossroads/dist/crossroads.min",
         "hasher":               "bower_modules/hasher/dist/js/hasher.min",
-        "signals":              "bower_modules/js-signals/dist/signals.min",
+        "jquery":               "bower_modules/jquery/dist/jquery",
+        "knockout":             "bower_modules/knockout/dist/knockout",
         "knockout-projections": "bower_modules/knockout-projections/dist/knockout-projections",
-
-        // TODO: Use Bower for these
-        "knockout": "app/lib/knockout-latest"
+        "signals":              "bower_modules/js-signals/dist/signals.min",
+        "text":                 "bower_modules/requirejs-text/text"
     },
     shim: {
         "bootstrap": { deps: ["jquery"] }
