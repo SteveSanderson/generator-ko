26207b1b3cc617b77ca78a58b8fa01210a489ef9
Steven Sanderson <steve@codeville.net>
2014-05-14 10:54:21 +0100

    Simplify HomeViewModel templates

--- app/templates/src/components/home-page/_home.js
+++ app/templates/src/components/home-page/_home.js
@@ -8,11 +8,6 @@ define(["knockout", "text!./home.html"], function(ko, homeTemplate) {
     this.message('You invoked doSomething() on the viewmodel.');
   };
 
-  HomeViewModel.prototype.dispose = function() {
-    // This runs when the component is torn down. Put here any logic necessary to clean up,
-    // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
-  };
-
   return { viewModel: HomeViewModel, template: homeTemplate };
 
 });
--- app/templates/src/components/home-page/_home.ts
+++ app/templates/src/components/home-page/_home.ts
@@ -8,9 +8,4 @@ export class viewModel {
     public doSomething() {
         this.message('You invoked doSomething() on the viewmodel.');
     }
-
-    public dispose() {
-        // This runs when the component is torn down. Put here any logic necessary to clean up,
-        // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
-    }
 }
